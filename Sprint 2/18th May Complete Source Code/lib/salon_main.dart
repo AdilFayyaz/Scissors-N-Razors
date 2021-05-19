7095bad95de5e671d6efc0093c1adf0735bf090b
AdilFayyaz <adilfayyaz6@gmail.com>
2021-05-19 16:26:40 +0500

    Sprint 2
    Added Login for Employees, UI for Salon Owner, and created an Employee Dashboard. So far, the dashboard does not have the Employee appointments.

---
+++ Sprint 2/18th May Complete Source Code/lib/salon_main.dart
@@ -0,0 +1,133 @@
+import 'customerDashboard.dart';
+
+import 'ManageSalon.dart';
+import 'addSalon.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'addSalonOwner.dart';
+import 'getInformation.dart';
+
+
+class salon extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Scissors & Razors',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  //final firestoreInstance = FirebaseFirestore.instance;
+  TextEditingController email = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title:Text(('Salon')),),
+        floatingActionButton: FloatingActionButton(child: Icon(Icons.add),
+          onPressed: (){
+            Navigator.push(context,MaterialPageRoute(builder: (_)=>addSalonOwner()));
+          },),
+        body: Center(child: Column(children: <Widget>[
+          Container(
+            margin: EdgeInsets.all(25),
+            child: FlatButton(
+              child: Text('SignUp', style: TextStyle(fontSize: 20.0),),
+              onPressed: () {
+                Navigator.push(context,MaterialPageRoute(builder: (_)=>addSalonOwner()));
+              },
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(border:Border.all()),
+            child: TextField(
+                controller: email,
+                decoration: InputDecoration(hintText: 'email')
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(25),
+            child: FlatButton(
+              child: Text('getSalonOwnerDetails', style: TextStyle(fontSize: 20.0),),
+              color: Colors.blueAccent,
+              textColor: Colors.white,
+              onPressed: () {
+                Navigator.push(context,MaterialPageRoute(builder: (_)=>getSalonOwner(email.text)));
+              },
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(25),
+            child: FlatButton(
+              child: Text('Add Salon', style: TextStyle(fontSize: 20.0),),
+              color: Colors.blueAccent,
+              textColor: Colors.white,
+              onPressed: () {
+                Navigator.push(context,MaterialPageRoute(builder: (_)=>addSalon(email.text)));
+              },
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(25),
+            child: FlatButton(
+              child: Text('customerDashboard', style: TextStyle(fontSize: 20.0),),
+              color: Colors.blueAccent,
+              textColor: Colors.white,
+              onPressed: () {
+                Navigator.push(context,MaterialPageRoute(builder: (_)=>customerDashboard(email.text)));
+              },
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(25),
+            child: FlatButton(
+              child: Text('Manage Salon', style: TextStyle(fontSize: 20.0),),
+              color: Colors.blueAccent,
+              textColor: Colors.white,
+              onPressed: () {
+                Navigator.push(context,MaterialPageRoute(builder: (_)=>manageSalon(email.text)));
+              },
+            ),
+          ),
+        ]
+        )
+        ));
+  }
+}
